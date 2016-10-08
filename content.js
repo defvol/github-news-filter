edc4b47fea090c58677cb09f29e766d31b6f6b9d
Rodolfo Wilhelmy <rwilhelmy@gmail.com>
2016-10-08 10:41:30 -0400

    Hide all issues on click

--- content.js
+++ content.js
@@ -5,5 +5,14 @@ chrome.runtime.onMessage.addListener(clickListener)
 function clickListener (request, sender, sendResponse) {
   if( request.message === 'clicked_browser_action' ) {
     console.log('and now you clicked the extension icon')
+    toggleFilter()
+  }
+}
+
+function toggleFilter () {
+  var issues = document.querySelectorAll('[class*=issues_]')
+  for (var i = 0, l = issues.length; i < l; ++i) {
+    var issue = issues[i]
+    issue.classList.toggle('hidden')
   }
 }
