536680992ba56cecff72c1c62f7e2398e7dfe8bd
Rodolfo Wilhelmy <rwilhelmy@gmail.com>
2016-10-08 10:42:21 -0400

    Remove logs

--- content.js
+++ content.js
@@ -1,12 +1,8 @@
-console.log('you are on GitHub')
 
 chrome.runtime.onMessage.addListener(clickListener)
 
 function clickListener (request, sender, sendResponse) {
-  if( request.message === 'clicked_browser_action' ) {
-    console.log('and now you clicked the extension icon')
-    toggleFilter()
-  }
+  if (request.message === 'clicked_browser_action') toggleFilter()
 }
 
 function toggleFilter () {
